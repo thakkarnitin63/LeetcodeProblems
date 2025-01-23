feff381157af2c7d016185f1948a3f548d2e948f
Nitin <thakkarnitin63@gmail.com>
2025-01-23 14:27:49 -0500

    GroupAnagrams

---
+++ Group_Anagrams.py
@@ -0,0 +1,59 @@
+"""
+Given an array of strings strs, group the 
+anagrams
+ together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+"""
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        map=defaultdict(list)
+        for str in strs:
+            hashkey=self.HashFun(str)
+            map[hashkey].append(str)
+
+        return list(map.values())
+
+    def HashFun(self,s):
+        """
+        Sort the array of content so we can get the exact pattern to map it    
+        """
+        return "".join(sorted(s))
+
+        
